58eb63c240340e78719a393180d773654944c456
Bellal Gazi <73124088+belalgaziBD1@users.noreply.github.com>
2024-01-26 02:40:05 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+include('include/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bazarbd</title>
+    <!--------CSS Custom ---------> 
+    <link rel='stylesheet'  href='/css/style.css?ver=<? $version ?>'>
+    
+    
+    <script src='Assets/main.js'></script>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <h2>Hello world</h2>
+    <h3>Hello world</h3>
+
+</body>
+</html>
