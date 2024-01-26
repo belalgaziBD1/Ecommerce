f7f738d42af071895bc2863a9e871bd766c075e5
belalgaziBD1 <belal.gazi@gmail.com>
2024-01-26 19:57:44 +0600

    navber add and Css

--- index.php
+++ index.php
@@ -9,20 +9,32 @@ include('include/config.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bazarbd</title>
     <!--------CSS Custom ---------> 
-    <link rel='stylesheet'  href='/css/style.css?ver=<? $version ?>'>
-    
-    
-    <script src='Assets/main.js'></script>
+    <link rel='stylesheet'  href='css/style.css?ver=<? $version ?>'>
+
+    <!--------CSS Icon Boxicons ---------> 
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+
+    <!--------CSS fontwesome --------->
+    <link rel='stylesheet'  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+   
 </head>
 <body>
-    <h1>Hello world</h1>
-    <h2>Hello world</h2>
-    <h3>Hello world</h3>
-    <div class="header">
-        <div class="nav">
-            
-        </div>
-    </div>
-
+<!------Header Start------>
+<header class="header">
+    <a href="#" class="logo"><i class='bx bx-cart-alt'> </i>Bazarbd</a>
+   
+    <nav class="navber">
+    <a href="#">Home</a>
+    <a href="#">Feature</a>
+    <a href="#">Products</a>
+    <a href="#">Categoriecs</a>
+    <a href="#">Blogs</a>
+    </nav>
+    
+</header>
+    
+ <!--------JS Custom --------->
+ <script src='js/main.js'></script>
 </body>
 </html>
